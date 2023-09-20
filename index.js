045aa9a0cc458245e73648a0f458f6619818ef77
smmazharu <hmoxygen@gmail.com>
2023-09-20 18:48:36 +0600

    create pipeline api to show in clientside chart data

--- index.js
+++ index.js
@@ -226,7 +226,40 @@ async function run() {
       })
     })
 
+    app.get('/order-stats', verifyJWT, verifyAdmin, async(req, res) =>{
+      const pipeline = [
+        {
+          $lookup: {
+            from: 'menu',
+            localField: 'menuItems',
+            foreignField: '_id',
+            as: 'menuItemsData'
+          }
+        },
+        {
+          $unwind: '$menuItemsData'
+        },
+        {
+          $group: {
+            _id: '$menuItemsData.category',
+            count: { $sum: 1 },
+            total: { $sum: '$menuItemsData.price' }
+          }
+        },
+        {
+          $project: {
+            category: '$_id',
+            count: 1,
+            total: { $round: ['$total', 2] },
+            _id: 0
+          }
+        }
+      ];
 
+      const result = await paymentCollection.aggregate(pipeline).toArray()
+      res.send(result)
+
+    })
 
 
     // Send a ping to confirm a successful connection
